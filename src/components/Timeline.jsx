5168d0038bc07dcdc5e64c4901d7cec1339b5344
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2024-04-29 17:31:14 -0400

    Refactor Timeline component to remove unnecessary CSS classes and update styles

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,17 +1,17 @@
 import INFO from "../data/user";
 const Timeline = () => {
   return (
-    <ol className="relative border-s border-gray-200 dark:border-gray-700 w-full md:w-1/2 mb-8 md:mb-0">
+    <ol className="relative border-s border-gray-200 w-full md:w-1/2 mb-8 md:mb-0">
       {INFO.education.map((education, index) => (
         <li key={index} className="mb-10 ms-4">
-          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-300 0">
             {education.date}
           </time>
-          <h3 className="text-lg font-semibold text-gray-700 dark:text-white">
+          <h3 className="text-lg font-semibold text-gray-500 ">
             {education.title}
           </h3>
-          <p className="mb-4 text-base font-normal text-gray-600 dark:text-gray-400">
+          <p className="mb-4 text-base font-normal text-gray-400">
             {education.description}
           </p>
         </li>
